b54357577562a716e2d522ebdccea6d64cbe09d6
Israel Damasceno <professorisrael92@gmail.com>
2021-05-09 01:10:06 -0300

    Add project files.

---
+++ src/OrderManager.Api/Controllers/AppBaseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace OrderManager.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppBaseController : ControllerBase
+    {
+        protected readonly IServiceProvider _serviceProvider;
+        public AppBaseController(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+    }
+}
---
+++ src/OrderManager.Api/Controllers/PedidoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace OrderManager.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidoController : AppBaseController
+    {
+        public PedidoController(IServiceProvider serviceProvider): base(serviceProvider)
+        {          
+        }
+        [HttpGet("Nomes")]
+        public IEnumerable<string> ListarNomes()
+        {
+            var nomes = new List<string>();
+            nomes.Add("Luiza");
+            nomes.Add("Israel");
+            nomes.Add("Elaine");
+            nomes.Add("Romario");
+            return nomes;
+        }
+    }
+}
